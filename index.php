56d21963097567661a06009eee47192f39c57ef8
kkid404 <vizzy1488@gmail.com>
2022-12-03 20:03:32 +0500

    started development

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main</title>
+</head>
+<body>
+    <header class="header">
+        <div class="logo__text">
+            <h1 class="main__text">Liberty Social Network</h1>
+            <p class="descript__text">uncensored, unrestricted</p>
+        </div>
+        <div class="header__btn">
+            <a href="#" target="_blank" class="sign_up">Sign up</a>
+            <a href="#" target="_blank" class="sign_in">Sign in</a>
+        </div>
+    </header>
+    <main class="main">
+        <div class="main__text">
+            <div class="container__paragraph">
+                <h3 class="paragraph__heading">Why  Fake?</h3>
+                <p class="paragrph__text">
+                    It's like a regular social network, but 
+                    without the personalities.  
+                    You can communicate on any topic without 
+                    worrying about social censure.  We're 
+                    committed to freedom, 
+                    so you can talk about anything you want. 
+                </p>
+            </div>
+            <div class="container__paragraph">
+                <h3 class="paragraph__heading">So I can talk about anything?</h3>
+                <p class="paragrph__text">
+                    Yes, you can discuss whatever you want. 
+                    Fake is an open source platform.  
+                    You can make sure that we don't store 
+                    IP addresses in a database.
+                </p>
+            </div>
+            <div class="container__paragraph">
+                <h3 class="paragraph__heading">So I can talk about anything?</h3>
+                <p class="paragrph__text">
+                    Yes, you can discuss whatever you want.  
+                    Fake is an open source platform.  
+                    You can make sure that we don't store 
+                    IP addresses in a database.
+                </p>
+            </div>
+            <div class="container__principles">
+                <h2 class="principles__heading"></h2>
+                <div class="principles__block">
+                    <h3 class="principles__heading">Anonymity</h3>
+                    <img src="./src/img/100.png" alt="anonimus">
+                    <p class="principles__text">
+                        The main principle of Fake is anonymity. 
+                        You can put your picture, but you should 
+                        not put your real name and surname.
+                    </p>
+                </div>
+                <div class="principles__block">
+                    <h3 class="principles__heading">Liberty</h3>
+                    <img src="./src/img/100.png" alt="liberty">
+                    <p class="principles__text">
+                        We are for freedom of thought. 
+                        You can be a Nazi, an alien, 
+                        or Vladimir Putin. All are welcome.
+                    </p>
+                </div>
+                <div class="principles__block">
+                    <h3 class="principles__heading">Anonymity</h3>
+                    <img src="./src/img/100.png" alt="anonimus">
+                    <p class="principles__text">
+                    We love everyone equally. 
+                    Fake is a living  organism created by its users.  
+                    Make yourself at home.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
