a40fc2586678e7e7b476fd2eeec38819eee33d7a
kkid404 <vizzy1488@gmail.com>
2022-12-03 21:14:08 +0500

    Added a font. Styled the header for the desktop.

--- index.php
+++ index.php
@@ -5,23 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Main</title>
+    <link rel="stylesheet" href="./src/css/fonts.css">
+    <link rel="stylesheet" href="./src/css/style.css">
+    <link rel="stylesheet" href="./src/css/null.css">
 </head>
 <body>
     <header class="header">
         <div class="logo__text">
-            <h1 class="main__text">Liberty Social Network</h1>
-            <p class="descript__text">uncensored, unrestricted</p>
+            <h1 class="heading__text heading__fonts">Anti Social Network</h1>
+            <p class="descript__text text__fonts">uncensored, unrestricted</p>
         </div>
-        <div class="header__btn">
-            <a href="#" target="_blank" class="sign_up">Sign up</a>
-            <a href="#" target="_blank" class="sign_in">Sign in</a>
+        <div class="header__btns">
+            <a href="#" target="_blank" class="header__btn text__fonts">Sign up</a>
+            <a href="#" target="_blank" class="header__btn text__fonts">Sign in</a>
         </div>
     </header>
     <main class="main">
         <div class="main__text">
             <div class="container__paragraph">
-                <h3 class="paragraph__heading">Why  Fake?</h3>
-                <p class="paragrph__text">
+                <h3 class="paragraph__heading heading__fonts">Why  Fake?</h3>
+                <p class="paragrph__text text__fonts">
                     It's like a regular social network, but 
                     without the personalities.  
                     You can communicate on any topic without 
@@ -31,8 +34,10 @@
                 </p>
             </div>
             <div class="container__paragraph">
-                <h3 class="paragraph__heading">So I can talk about anything?</h3>
-                <p class="paragrph__text">
+                <h3 class="paragraph__heading heading__fonts">
+                    So I can talk about anything?
+                </h3>
+                <p class="paragrph__text text__fonts">
                     Yes, you can discuss whatever you want. 
                     Fake is an open source platform.  
                     You can make sure that we don't store 
@@ -40,8 +45,10 @@
                 </p>
             </div>
             <div class="container__paragraph">
-                <h3 class="paragraph__heading">So I can talk about anything?</h3>
-                <p class="paragrph__text">
+                <h3 class="paragraph__heading heading__fonts">
+                    So I can talk about anything?
+                </h3>
+                <p class="paragrph__text text__fonts">
                     Yes, you can discuss whatever you want.  
                     Fake is an open source platform.  
                     You can make sure that we don't store 
@@ -51,27 +58,27 @@
             <div class="container__principles">
                 <h2 class="principles__heading"></h2>
                 <div class="principles__block">
-                    <h3 class="principles__heading">Anonymity</h3>
+                    <h3 class="principles__heading heading__fonts">Anonymity</h3>
                     <img src="./src/img/100.png" alt="anonimus">
-                    <p class="principles__text">
+                    <p class="principles__text text__fonts">
                         The main principle of Fake is anonymity. 
                         You can put your picture, but you should 
                         not put your real name and surname.
                     </p>
                 </div>
                 <div class="principles__block">
-                    <h3 class="principles__heading">Liberty</h3>
+                    <h3 class="principles__heading heading__fonts">Liberty</h3>
                     <img src="./src/img/100.png" alt="liberty">
-                    <p class="principles__text">
+                    <p class="principles__text text__fonts">
                         We are for freedom of thought. 
                         You can be a Nazi, an alien, 
                         or Vladimir Putin. All are welcome.
                     </p>
                 </div>
                 <div class="principles__block">
-                    <h3 class="principles__heading">Anonymity</h3>
+                    <h3 class="principles__heading heading__fonts">Anonymity</h3>
                     <img src="./src/img/100.png" alt="anonimus">
-                    <p class="principles__text">
+                    <p class="principles__text text__fonts">
                     We love everyone equally. 
                     Fake is a living  organism created by its users.  
                     Make yourself at home.
